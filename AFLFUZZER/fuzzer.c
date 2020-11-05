15031f1de4c60e58545976eedb870177919bf7de
tao lei <yeshen4328@163.com>
2020-11-05 17:20:03 +0800

    using honggfuzz

--- AFLFUZZER/fuzzer.c
+++ AFLFUZZER/fuzzer.c
@@ -1,100 +1,70 @@
-#include <stdio.h>
+#include <stdio.h>  
+#include <unistd.h>  
+#include <sys/types.h>  
+#include <sys/stat.h>  
+#include <sys/fcntl.h>   
+
 #include <pthread.h>
 #include <stdlib.h>
-#include <unistd.h>
+
 #include <errno.h>
-#include <stdio.h>
+
 #include <signal.h>
 #include <fcntl.h>
 #include <sys/select.h>
 #include <sys/time.h>
-#include <sys/types.h>
 #include <string.h>
 #define READ 0 
-#define WRITE 1
-void handle(int sig)
-{
-    exit(0);
-}
-
-int main(int argc,char *argv[])
-{
- 
-    //开始io劫持
+#define WRITE 1 
+int main(int argc, char const *argv[])  
+{  
+	if (argc != 2)  
+   {  
+		printf("null args!\n");  
+		return -1;  
+    }  
+	
+	/* Get file state */  
+    struct stat fstat;  
+    if (stat(argv[1], &fstat))  
+    {  
+        printf("Failed ^_^\n");  
+        return -1;  
+    }  
+	
+    /* Open file */  
     int pipe_in[2];
     int pipe_out[2];
     if (pipe(pipe_in)) err(1, "pipe_in");
     if (pipe(pipe_out)) err(1, "pipe_out");
-    //fcntl(pipe_out[READ], F_SETFL, O_NONBLOCK);
-    fd_set readfd;
-    fd_set writefd;
-    struct timeval timeout;
-    signal(SIGCHLD, handle);  
-    char out_buf[1000] = {};
-    char buff2[1000] = {};
+    FILE * fd = NULL;  
+    fd = open(argv[1], O_RDONLY);  
+    if (fd == -1)  
+    {  
+        printf("open file failed!\n");  
+        return -1;  
+    }  
+
     int pid = fork();
     
     if(pid == 0)
     {
         dup2(pipe_out[WRITE], 1);//redirect stdout to pipe_out
         dup2(pipe_in[READ], 0);//redirect stdin to pipe_in
-        system("./freenote2");
+        system("./level0");
     }
-    
-    // read(pipe_out[READ], out_buf, 1000);
-    // printf("%s",out_buf);
-    // memset(out_buf, 0, 1000);
-    int ret = 0;
-    
-    timeout.tv_sec = 5*60;
-    timeout.tv_usec = 1;
-    
-    int status = 0;
-    int exit = 0;
-    
-    while(exit != -1)
-    {
-        FD_ZERO(&readfd);                       /* 清空文件描述符集合 */
-        FD_SET(0, &readfd);                 /* 添加文件描述符集合 */
-        FD_SET(pipe_out[READ], &readfd);      /* 添加文件描述符集合 */
-        //FD_SET(log, &writefd);                  /* 添加文件描述符集合 */
-        ret = select(pipe_out[READ] + 1, &readfd, NULL, NULL, &timeout);
-        
-        switch(ret)
-        {
-            case -1:
-            {
-                printf("error\n");
-                break;
-            }
-            case 0:
-            {
-                printf("time out\n");
-                break;
-            }
-            default:
-            {
-                if(FD_ISSET(pipe_out[READ], &readfd)>0)
-                {
-                    ret = read(pipe_out[READ], out_buf, 1000-1);//read from pipe/sub process
-   
-                    if(ret > 0)      
-                        write(1, out_buf, ret);
-                }
-                else if(FD_ISSET(0, &readfd)>0)
-                {
-                    int s = 0;
-                    ret = read(0, buff2, 1000-1);//read from stdin
-                    
-                    if(ret > 0)
-                    {
-                        write(pipe_in[WRITE], buff2, ret);
-                    }
-                }
-            }
-        }
-        memset(buff2, 0, 1000);
-        memset(out_buf, 0, 1000);
-    }
+
+    /* Select */  
+    char buf[4096];
+    int ret = 0;  
+    if ((ret = read(fd, buf, 4096)) == -1)  
+    {  
+        printf("read failed!");  
+        return -1;  
+    }  
+    // write(1, "hello", ret);
+    //write(1, buf, ret);
+    write(pipe_in[WRITE], buf, ret);
+
     return 0;
 }
